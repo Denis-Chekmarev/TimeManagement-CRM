e3c00a157a86de70a4ddb18d731c7ec142bbc1a4
Denis <chekmarevdenchik5@gmail.com>
2022-04-20 15:52:18 +0700

    first touch with tkinert

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+root['bg'] = '#fafafa'
+root.title('TimeManagement')
+root.wm_attributes('-alpha', 0.85)
+root.geometry('300x250')
+
+root.resizable(width=False, height=False)
+
+root.mainloop()
